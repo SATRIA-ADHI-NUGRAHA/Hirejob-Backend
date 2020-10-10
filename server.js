0c181cbe3b496e2110f3dfe8c9a9b1cb19b2c07b
rifqyandika <andikakiky87@gmail.com>
2020-10-10 11:53:12 +0700

    res

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const server = http.createServer(app)
 // const io = require(server)
 const env = require('./src/helpers/env')
 const db = require('./src/config/config')
+const user = require('./src/routers/user')
 
 db.connect((err) => {
     if(err) throw err
     console.log('Database connected');
 })
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/v1/user', user)
 
 server.listen(env.PORT, () => {
     console.log(`Server running at port ${env.PORT}`);
