c75206f7bb071d30dc264c7830fe024975a832de
unknown <satriaadhinugraha05@gmail.com>
2020-10-10 11:40:15 +0700

    response

---
+++ src/routers/company.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const companyControllers = require('../controllers/companyController')
+const router = express.Router()
+const upload = require('../helper/upload')
+
+
+router
+.get('/getall',companyControllers)
+.get('/detail/:id_company',companyControllers)
+.post('/insert', companyControllers)
+.put('/update/:id_company',upload.single('image_com'),companyControllers)
+.delete('/delete/:id_company',companyControllers)
+
+module.exports = router
